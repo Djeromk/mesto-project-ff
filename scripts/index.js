76328b6d54b496423382eafab242affb5ef34e00
Alexey Surkov <alexprakticum@yandex.ru>
2024-07-23 16:32:15 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function deleteCard(e) {
+  const card = e.target.closest("li");
+  card.remove();
+}
 
-// @todo: Функция создания карточки
+function addCard(cardData, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const removeButton = cardItem.querySelector(".card__delete-button");
+  cardItem.querySelector(".card__image").setAttribute("src", cardData.link);
+  cardItem.querySelector(".card__title").append(cardData.name);
+  removeButton.addEventListener("click", deleteCard);
+  placesList.append(cardItem);
+}
 
-// @todo: Функция удаления карточки
+initialCards.forEach((card) => addCard(card, deleteCard));
 
-// @todo: Вывести карточки на страницу
