e141186369c45f8d972fa90532eab4477ed3fd05
Alexey Surkov <alexprakticum@yandex.ru>
2024-07-24 10:59:24 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,21 @@
 const placesList = document.querySelector(".places__list");
 
 function deleteCard(e) {
-  const card = e.target.parentElement;
+  const card = e.target.closest(".places__item");
   card.remove();
 }
 
-function addCard(cardData, deleteCard) {
+function createCard(cardData, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
   const removeButton = cardItem.querySelector(".card__delete-button");
   cardItem.querySelector(".card__image").setAttribute("src", cardData.link);
   cardItem.querySelector(".card__title").append(cardData.name);
   removeButton.addEventListener("click", deleteCard);
-  placesList.append(cardItem);
+  return cardItem;
 }
 
-initialCards.forEach((card) => addCard(card, deleteCard));
-
+initialCards.forEach((card) => {
+  const cardElement = createCard(card, deleteCard);
+  placesList.append(cardElement);
+});
